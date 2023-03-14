342bea905c7586768e204746c811c05d876e4109
Sada Sow <sadasow@Sadas-M1-MacBook-Pro.local>
2023-03-14 14:21:47 +0100

    details

--- poke-app-client/src/App.js
+++ poke-app-client/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PokeDetails } from "./PokeDetails";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} />
+          <Route path={"/user_pokemon"} />
+          <Route path={"/add_pokemon"} />
+          <Route path={"/update/:id"} />
+        </Routes>
+      </BrowserRouter>
+      <PokeDetails />
+    </div>
+  );
 }
 
 export default App;
