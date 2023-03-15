edc8f982a95186b7d6f5903e91f45ecfeeda4b83
Wiebke <wiebke.feldt@gmail.com>
2023-03-15 23:02:07 +0100

    added external api call

--- poke-app-backend/controllers/pokemonsExternal.js
+++ poke-app-backend/controllers/pokemonsExternal.js
@@ -1,33 +1,41 @@
 const axios = require("axios");
 
 const getAllExternalPokemons = async (req, res) => {
-  const limit = 3;
-  let pokemons = [];
+  const { page = 1, docsPerPage = 6 } = req.query;
 
-  for (let elem = 1; elem < limit; elem++) {
-    axios
-      .get(`//pokeapi.co/api/v2/pokemon/${elem}`)
-      .then((response) => {
-        const pokemonObject = {
-          name: response.data.name,
-          pokeType: response.data.types[0].type.name,
-          image: response.data.sprites.other["official-artwork"].front_default,
-          attacks: [
-            response.data.moves[0].move.name,
-            response.data.moves[1].move.name,
-            response.data.moves[2].move.name,
-          ],
-        };
-        pokemons.push(pokemonObject);
-        //console.log(pokemons);
-      })
-      .catch((err) => console.log(err))
-      .finally((res) => {
-        console.log("mmmmm", pokemons);
-      });
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+  const urls = [];
+  for (
+    let elem = (page - 1) * docsPerPage + 1;
+    elem < page * docsPerPage;
+    elem++
+  ) {
+    urls.push(`${baseUrl}${elem}`);
+    console.log(`${baseUrl}${elem}`);
   }
-  console.log("grrrr", pokemons);
-  res.json(pokemons);
+
+  try {
+    const rawPokemons = await Promise.all(
+      urls.map((url) => fetch(url).then((response) => response.json()))
+    );
+    const pokemons = rawPokemons.map((rawPokemon) => {
+      console.log(rawPokemon);
+
+      return {
+        name: rawPokemon.name,
+        pokeType: rawPokemon.types[0].type.name,
+        image: rawPokemon.sprites.other["official-artwork"].front_default,
+        attacks: [
+          rawPokemon.moves[0].move.name,
+          rawPokemon.moves[1].move.name,
+          rawPokemon.moves[2].move.name,
+        ],
+      };
+    });
+    res.json(pokemons);
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
 module.exports = {
