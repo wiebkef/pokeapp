be887837825b4014c104312c57bde1c290ee4358
unknown <aysamuel007@gmail.com>
2023-03-16 12:59:38 +0100

    pokeys

--- poke-app-client/src/App.js
+++ poke-app-client/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
-
+import { PokeDetails } from "./PokeDetails";
+import Homedisplay from "./components/Homedisplay";
 import Navbar from "./components/Navbar";
 import { AddPokemon } from "./AddPokemon";
 
@@ -8,10 +9,12 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <Hero />
+      <Homedisplay />
 
       <Routes>
         <Route path={"/"} element={<Home />} />
-        <Route path={"/user_pokemon"} />
+        <Route path={"/user_pokemon/"} />
         <Route path={"/add_pokemon"} element={<AddPokemon />} />
         <Route path={"/update/:id"} />
       </Routes>
