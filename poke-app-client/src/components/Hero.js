7d1480ac0f8d52ee4df23f736a7312b3431a2320
unknown <aysamuel007@gmail.com>
2023-03-17 16:41:15 +0100

    newdesign

--- poke-app-client/src/components/Hero.js
+++ poke-app-client/src/components/Hero.js
@@ -1,14 +1,18 @@
-import React from "react";
-import Pokemon from "../assets/pokemon.png";
-import Typed from "react-typed";
+import React from 'react';
+import Pokemon from '../assets/pokemon.png';
+import Typed from 'react-typed';
 
 function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <img className="object-contain h-50 w-50" src={Pokemon} alt="/" />
+    <div className='flex flex-col justify-center items-center py-4'>
+      <img
+        className='object-contain h-50 w-50'
+        src={Pokemon}
+        alt='/'
+      />
       <Typed
-        className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
-        strings={["Catch them All"]}
+        className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+        strings={['Catch them All']}
         typeSpeed={120}
         backSpeed={140}
         loop
